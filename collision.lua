146bdd335eab7665db878b700df44d4febd64080
opfez <opfez@disroot.org>
2022-02-20 22:04:24 +0100

    asdflsaf

--- collision.lua
+++ collision.lua
@@ -2,5 +2,5 @@ function collide_check(player, box)
     return player.pos.x < box.pos.x + box.width and
         player.pos.x + player.width > box.pos.x and
         player.pos.y < box.pos.y + box.height and
-        player.pos.y + player1.height > player2.y
+        player.pos.y + player.height > box.pos.y
 end
