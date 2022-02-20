5a0127f3e9caa2aa08eedb069802c00cf8dfc2c4
opfez <opfez@disroot.org>
2022-02-20 20:47:26 +0100

    initial

---
+++ conf.lua
@@ -0,0 +1,8 @@
+WIDTH = 640
+HEIGHT = 640
+
+function love.conf(t)
+    t.window.title = "Milos"
+    t.window.height = WIDTH
+    t.window.width = HEIGHT
+end
---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.print("hello", WIDTH/2, HEIGHT/2)
+end
