24c730b1cae1d2fb3b4d02eafcc98db11bac695c
Legenderox <brain1127@outlook.com>
2022-02-20 22:03:58 +0100

    resolved conflic

--- main.lua
+++ main.lua
@@ -1,14 +1,16 @@
-require("character")
+character = require("character")
+worldgen = require("worldgen")
 
 function love.load()
-
+    
 end
 
 function love.update(dt)
-    update_player(dt)
+    
 end
 
 function love.draw()
     love.graphics.print(player.pos.y, 0, 0)
     love.graphics.draw(player.sprite, player.pos.x, player.pos.y)
+    drawLevel(1)
 end
